f4f8bb4183d06eeb4e0d62ade002b55eb954fc88
Mahongyu <241626521@qq.com>
2025-04-26 15:46:23 +0800

    update InsertionSort

--- Unordered_array_generation.py
+++ Unordered_array_generation.py
@@ -6,3 +6,4 @@ unordered_array = [random.randint(1, 1000) for _ in range(100)]
 # 打印前10个元素（避免输出过长）
 print("生成的无序数组（前10个元素）:", unordered_array[:10])
 print("数组长度:", len(unordered_array))
+print(unordered_array)
